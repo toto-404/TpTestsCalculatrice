1643e86c01bbf64b7a2a5ae9fd1944fc343483fc
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-04-02 15:51:15 +0200

    test methode soustraction calculatricetest.cs

---
+++ TpTestsCalculatrice.Tests/CalculatriceTest.cs
@@ -0,0 +1,20 @@
+﻿namespace TpTestsCalculatrice.Tests;
+
+[TestClass]
+public class CalculatriceTests
+{
+    [TestMethod]
+    public void Addition_Retourne5_Si2Plus3()
+    {
+        Calculatrice calc = new Calculatrice();
+        int resultat = calc.Addition(2, 3);
+        Assert.AreEqual(5, resultat);
+    }
+    [TestMethod]
+    public void Soustraction_Retourne2_Si5Moins3()
+    {
+        Calculatrice calc = new Calculatrice();
+        int resultat = calc.Soustraction(5, 3);
+        Assert.AreEqual(2, resultat);
+    }
+}
